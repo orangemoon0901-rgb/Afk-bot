b78cbeab68e65ff803362e6d58ee1c0726ce234f
orangemoon0901-rgb <orangemoon0901@gmail.com>
2026-08-07 14:44:07 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,7 +103,7 @@ function createBot() {
       setTimeout(() => {
         if (!bot) return;
 
-        bot.chat("/login prasadpradeep2009");
+        bot.chat("/login SANAP@2009");
 
         setTimeout(() => {
           if (!bot) return;
